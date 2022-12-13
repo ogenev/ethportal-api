5ddba66b62d8cb1d68282a2c57857f2460c08464
Ognyan Genev <ognian.genev@gmail.com>
2022-12-13 17:54:32 +0200

    Add discv5 nodeInfo endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
+mod discv5;
+pub mod types;
 mod web3;
 
+pub use discv5::*;
 pub use web3::*;
 
 mod prelude {
+    pub use crate::types;
     pub use jsonrpsee::core::RpcResult;
     pub use jsonrpsee::proc_macros::rpc;
+    pub use serde::{Deserialize, Deserializer, Serialize, Serializer};
 }
